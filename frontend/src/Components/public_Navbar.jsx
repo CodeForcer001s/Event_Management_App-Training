c95f8d99426a5775077c8f90e08b46c7a9e3ea5a
S Jegannath <sjegannath56@gmail.com>
2025-02-25 22:19:42 +0530

    second draft

---
+++ frontend/src/Components/public_Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import './publicnavbar.css'
+const public_Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default public_Navbar
---
+++ frontend/src/Pages/publics/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import public_Navbar from '../../Components/public_Navbar'
+
+const LandingPage = () => {
+  return (
+    <div>
+      <public_Navbar/>
+    </div>
+  )
+}
+
+export default LandingPage
